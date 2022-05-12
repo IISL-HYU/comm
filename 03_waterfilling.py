5ae08514961ca0c9875c6db3efb8258cc0750a44
Sungweon (Sam) Hong <hongsw94@hanyang.ac.kr>
2022-05-12 14:08:10 +0900

    waterfilling revised

--- 03_waterfilling.py
+++ 03_waterfilling.py
@@ -8,7 +8,7 @@ SNR_dB = 10
 P = 10 ** (SNR_dB/10) # Sum Power budgent defined via the SNR 
 
 # The channel specific gains drawn from Gaussian distribution 
-g = np.abs(np.random.randn(N, 1))
+g = 2 * np.abs(np.random.randn(N, 1))
 step = 0
 alphas = []
 caps = []
@@ -19,7 +19,7 @@ stop_threshold = 1e-5
 
 while(np.abs(alpha_low-alpha_high) > stop_threshold):
     alpha = (alpha_low + alpha_high) / 2 
-    alphas.append(float(alpha))
+
     # solve the power allocation 
     p = 1/alpha - N0/g
     p[p < 0] = 0 
@@ -29,6 +29,7 @@ while(np.abs(alpha_low-alpha_high) > stop_threshold):
     else: 
         alpha_high = alpha 
     step += 1 
+    alphas.append(float(alpha))
     caps.append(np.sum(np.log(1 + g * p / N0)))
 # Print the achievable rate in nats/s 
 print(np.sum(np.log(1 + g * p / N0)))
@@ -38,9 +39,10 @@ plt.figure(1)
 plt.bar(range(10), height=N0/g.squeeze(), width=1, label='Channel')
 plt.bar(range(10), height= -1 * p.squeeze(), width=1, bottom=1/alpha, label='Power allocation')
 plt.xticks(range(10))
+plt.axis([-1, 10, 0, 10])
 plt.xlabel("Channel number")
-plt.ylabel("Gain")
-plt.title("Gains per channel")
+plt.ylabel("SNR")
+plt.title("SNR per channel")
 plt.axhline(y=1 / alpha, color="r", linewidth=1, label='1/lambda')
 plt.legend()
 plt.figure(2)
