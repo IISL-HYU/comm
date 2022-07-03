2f30fe4ef2324fc7633c2bfde625e872a59f3d8f
S.W Hong <hongsw94@hanyang.ac.kr>
2022-07-04 01:08:21 +0900

    Add Modulation

--- modulation/modulator.py
+++ modulation/modulator.py
@@ -1,27 +1,47 @@
+# Modulation Class (should be inherited)
 import numpy as np 
 
 class Modulator():
-    def __init__(self, M):
+    def __init__(self, M, normalize=True):
         self.M = M
         self.k = int(np.log2(M)) 
+        self.normalize = normalize
+        self.mapping = dict()
+        self.demapping = dict()
 
-    def map(self, bits: np.ndarray):
+    def modulate(self, bits: np.ndarray):
+        """
+        This method should be overriden
+        """
         pass 
 
-    def demap(self, symbols: np.ndarray):
+    def demodulate(self, symbols: np.ndarray):
+        """
+        This method should be overriden
+        """
         pass 
 
     def dec2bin(self, x: np.ndarray):
-        """Decimal array to Binary arrays(string format)"""
-        bins = []
-        for dec in x:
-            bcw = str(bin(dec)[2:]).zfill(self.k)
-            bins.append(bcw)
-        return np.array(bins)
+            """Decimal array to Binary arrays(string format)"""
+            bins = []
+            for dec in x:
+                bcw = list(str(bin(dec)[2:]).zfill(self.k))
+                bins.append(bcw)
+            return np.array(bins, dtype=np.int32)
 
     def bin2dec(self, bins: np.ndarray):
         """Binary arrays(string format) to decimal arrays"""
-        decs = np.zeros_like(bins, dtype=np.int32)
+        decs = np.zeros(shape=(len(bins)), dtype=np.int32)
         for i, b in enumerate(bins):
+            b = ''.join(str(bit) for bit in b)
             decs[i] = int(b, 2)
         return decs
+
+    def get_map(self):
+        return self.mapping
+    
+    def get_demap(self):
+        return self.demapping
+
+    def __call__(self):
+        return self.mapping
--- modulation/qam.py
+++ modulation/qam.py
@@ -1,9 +1,12 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 from modulator import Modulator
 
 class QAM(Modulator):
     def __init__(self, M=16):
+        # inherit Modulator class 
         super().__init__(M)
+        
         self.axis_points = self._make_axis_point(M)
         
         # integer 0 ~ M-1
@@ -14,10 +17,62 @@ class QAM(Modulator):
         self.symbols = self._make_symbols()
 
         # Mapping dictionary for M-QAM modulation
-        self.mapping = dict(zip(self.ipBin, self.symbols))
+        self.mapping = dict()
+        for b, s in zip(self.ipBin, self.symbols):
+            self.mapping[tuple(b)] = s
+        self.demapping = {v: k for k, v in self.mapping.items()}
+        # symbol power normalization (E[|x|^2] = 1)
+        if self.normalize:
+            self.avg_power = self._compute_avg_power()
+            self.mapping = {k: v/self.avg_power for k, v in self.mapping.items()}
+        else: 
+            self.avg_power = 1
 
         # Demapping dictionary for M-QAM demodulation 
-        self.demapping = {v: k for k, v in self.mapping.items()}
+
+    def modulate(self, bits: np.ndarray):
+        idx = np.arange(len(bits) // self.k, dtype=np.int32)
+        symstream = np.zeros(shape=(len(idx),), dtype=np.complex64)
+
+        for i in idx:
+            sym = tuple(bits[self.k * i : self.k * (i+1)])
+            symstream[i] = self.mapping[sym]
+        return symstream
+    
+    def demodulate(self, symbols: np.ndarray):
+            
+        symbols *= self.avg_power
+
+        rounds = np.fromiter(self.demapping.keys(), dtype=np.complex64)
+        rounds = np.unique(rounds) 
+
+        compare = np.subtract.outer(symbols, rounds)
+        s_hat = rounds[np.argmin(abs(compare), axis=1)]
+
+        messages_recovered = self.demap(s_hat)
+        return messages_recovered
+
+    def demap(self, symbols: np.ndarray):
+        """Returns an 1-dimensional array of bitstream"""
+        bitstream_recovered = []
+        demap = self.get_demap()
+        for sym in symbols:
+            bitstream_recovered.append(list(demap[sym]))
+        return np.array(bitstream_recovered).reshape(-1, 1).squeeze()
+
+    def plot_constellation_points(self, ):
+        plt.figure(figsize=(8, 8))
+        for b3 in [0, 1]:
+            for b2 in [0, 1]:
+                for b1 in [0, 1]:
+                    for b0 in [0, 1]:
+                        B = (b3, b2, b1, b0) 
+                        Q = self.mapping[B]
+                        plt.plot(Q.real, Q.imag, 'bo', zorder=10)
+                        plt.text(Q.real, Q.imag+0.1, "".join(str(x) for x in B), ha='center')
+        plt.axis([-1.5, 1.5, -1.5, 1.5])
+        plt.grid(True, linestyle="--")
+        plt.title(f"Constellation Mapping for {self.M}-QAM")
 
  
     def _make_symbols(self):
@@ -41,10 +96,17 @@ class QAM(Modulator):
         p_num = int(np.sqrt(M))
         return np.linspace(con_min, con_max, p_num)
 
-    def __call__(self):
-        return self.mapping
+    def _compute_avg_power(self,):
+        avgpower = [abs(i)**2 for i in self.mapping.values()]
+        avgpower = np.sqrt(np.average(avgpower))
+        return avgpower
 
 
 if __name__ == "__main__":
     qam = QAM(M=16)
-    print(qam())
+    mapping_table = qam.get_map()
+    demapping_table = qam.get_demap() 
+
+    print(mapping_table) 
+
+    print(demapping_table)
