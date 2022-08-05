2c9b9475bce4f5e20506854a0d0453cf665cbe9c
성원(Sam) 홍 <sungweon-hong@seong-wonSams-MacBook-Pro.local>
2022-08-05 18:58:51 +0900

    Add 1-bit-ADC.py

---
+++ 1-bit-ADC.py
@@ -0,0 +1,13 @@
+
+# Multi-User uplink MIMO with 1-bit ADC 
+# written by Sungweon Hong (SAM) 
+# Hanyang university, Seoul, Korea
+
+
+# Import packages
+import numpy as np 
+import matplotlib.pyplot as plt 
+import random
+import os 
+
+
