9248455a69cd99051ddd8bceecfe6a0053a51930
Sungweon (Sam) Hong <hongsw94@hanyang.ac.kr>
2022-08-11 14:39:34 +0900

    add make_symbol_space for ML detection

--- detector.py
+++ detector.py
@@ -1,6 +1,12 @@
 
 import numpy as np 
 
+def make_symbol_space(M, K, constellation_points):
+    # make symbol space for ML detection
+    index_space = np.indices([M for _ in range(K)]).reshape(K, -1).T 
+    symbol_space = np.take(constellation_points, index_space)
+    return symbol_space 
+
 def symbol_by_symbol(x_tilde, constellation_points):
     compare = np.abs(x_tilde - constellation_points)
     det_by_idx = np.argmin(compare, axis=-1)
