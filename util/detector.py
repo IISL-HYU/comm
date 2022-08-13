d7282c956b5ad5187b1536f14265b2952505ebd0
성원(Sam) 홍 <sungweon-hong@seong-wonSams-MacBook-Pro.local>
2022-08-13 16:08:04 +0900

    nML on going

--- util/detector.py
+++ util/detector.py
@@ -26,9 +26,9 @@ def ml(y_re, H_re, symbol_space, snr_dB):
     # complex to real and then reshape for broadcast
     snr = 10**(snr_dB/20)
     T = H_re.shape[0]
-    M = symbol_space.shape[-1]
-    K = H_re.shape[-1]
-    x_shape = tuple(T, K, 1)
+    K = H_re.shape[-1] // 2
+    M = int(symbol_space.shape[0] ** (1/K))
+    x_shape = tuple([T, K, 1])
     symbol_space_re = np.hstack((symbol_space.real, symbol_space.imag)) * np.ones(shape=(T, M**K, 2*K))
     likelihood = phi(np.matmul(symbol_space_re, H_re.transpose(0, 2, 1)) * y_re.transpose(0, 2, 1) * np.sqrt(2 * snr))
     log_likelihood = np.sum(np.log(likelihood), axis=2)
