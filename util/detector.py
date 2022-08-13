c798fd37715c3ef7b60fcdf1b68ad79df7af4502
성원(Sam) 홍 <sungweon-hong@seong-wonSams-MacBook-Pro.local>
2022-08-13 15:33:57 +0900

    Add explanation to detector.py

--- util/detector.py
+++ util/detector.py
@@ -2,18 +2,27 @@ from core.core import *
 import numpy as np 
 
 def make_symbol_space(M, K, constellation_points):
-    # make symbol space for ML detection
+    """
+    Makes symbol space in case of Maximum Likelihood detection: with modulation level M and K users(transmit antenna) 
+    """
     index_space = np.indices([M for _ in range(K)]).reshape(K, -1).T 
     symbol_space = np.take(constellation_points, index_space)
     return symbol_space 
 
 def symbol_by_symbol(x_tilde, constellation_points):
+    """
+    symbol-by-symbol detection with respect to each antenna: 
+    can be used when relaxation and estimation is preceded (1-bit-ADC case) 
+    """
     compare = np.abs(x_tilde - constellation_points)
     det_by_idx = np.argmin(compare, axis=-1)
     x_hat = np.take(constellation_points, det_by_idx).reshape(x_tilde.shape)
     return x_hat
 
 def ml(y_re, H_re, symbol_space, snr_dB):
+    """
+    Maximum Likelihood detection for KxN mu-MIMO channel (uses Gaussian distribution) 
+    """
     # complex to real and then reshape for broadcast
     snr = 10**(snr_dB/20)
     T = H_re.shape[0]
